222c3f1308df618233838b4a627c794c17268c7c
Daniel <daniel.minior.pk@gmail.com>
2024-02-08 01:13:18 +0100

    Fixing CORS error

--- backend/src/main/java/pl/dminior8/movies/Controller/MovieController.java
+++ backend/src/main/java/pl/dminior8/movies/Controller/MovieController.java
@@ -1,6 +1,7 @@
 package pl.dminior8.movies.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +22,10 @@ public class MovieController {
     private MovieService movieService;
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Access-Control-Allow-Origin", "*");
         return new ResponseEntity<List<Movie>>(
-                movieService.allMovies(), HttpStatus.OK);
+                movieService.allMovies(),headers, HttpStatus.OK);
     }
     @GetMapping("/{imdbId}")
     public ResponseEntity<Optional<Movie>> getSingleMovie(
